0cf14c32fe8837388190ff949234d8a54543df75
Gokul <106098368+Gokul07013@users.noreply.github.com>
2024-08-01 09:22:14 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-CONTAINER_ID=docker ps | awk 'NR>1 {print $1}'
+CONTAINER_ID=$(sudo docker ps | awk 'NR>1 {print $1}')
 
 docker stop $CONTAINER_ID
