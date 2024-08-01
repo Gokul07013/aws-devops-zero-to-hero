650201ffd06abe887883a7cb9ce69210b9cb478e
Gokul <106098368+Gokul07013@users.noreply.github.com>
2024-08-01 10:09:44 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
+echo "Stopping containers."
+
 # Get the container IDs of all running containers
-CONTAINER_IDS=$(docker ps -q)
+CONTAINER_IDS=$(sudo docker ps -q)
 
 # Debugging output: Print the container IDs to verify
 echo "Container IDs: '$CONTAINER_IDS'"
@@ -10,7 +12,7 @@ echo "Container IDs: '$CONTAINER_IDS'"
 # Check if there are any running containers
 if [ -n "$CONTAINER_IDS" ]; then
   echo "Stopping containers: $CONTAINER_IDS"
-  docker stop $CONTAINER_IDS
+  sudo docker stop $CONTAINER_IDS
 else
   echo "No running containers found."
 fi
